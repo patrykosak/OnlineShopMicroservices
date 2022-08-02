8bb925f03d54a11b8bdaf772b50dfa77322c1171
Patryk Osak <posak@sii.pl>
2022-08-02 12:25:37 +0200

    order service implementation

---
+++ order-service/src/main/java/com/shop/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.shop.orderservice.controller;
+
+import com.shop.orderservice.dto.OrderRequest;
+import com.shop.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-service/src/main/java/com/shop/orderservice/dto/OrderResponse.java
@@ -0,0 +1,4 @@
+package com.shop.orderservice.dto;
+
+public class OrderResponse {
+}
