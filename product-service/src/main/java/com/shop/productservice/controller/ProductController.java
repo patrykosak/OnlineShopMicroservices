50d2d134c3811173d77df66cd51a9fc9c14eb69a
Patryk Osak <posak@sii.pl>
2022-08-02 09:15:21 +0200

    product service

--- product-service/src/main/java/com/shop/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/shop/productservice/controller/ProductController.java
@@ -1,9 +1,29 @@
 package com.shop.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.shop.productservice.dto.ProductRequest;
+import com.shop.productservice.dto.ProductResponse;
+import com.shop.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
 class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
 }
